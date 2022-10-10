36290699b8ddbd818654233f5057c470adb975b8
Tjaart van der Walt <git@tjaart.org>
2022-10-10 14:00:35 -0700

    Add uploader

---
+++ delta/uploader.py
@@ -0,0 +1,44 @@
+import os
+import threading
+from time import sleep
+
+import requests
+
+from delta import logging
+from delta.collector_queue import q
+
+
+class Uploader:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.api_token = os.environ.get("DELTA_API_TOKEN")
+        self.api_url = (
+            os.environ.get("DELTA_API_URL") or "http://localhost:4000/api/upload"
+        )
+
+    def uploader(self):
+        while True:
+            size = q.size
+            try:
+                items = []
+                for i in range(0, size):
+                    items.append(q.get())
+
+                self.logger.debug(items)
+
+                self.__upload__(items)
+            except Exception as e:
+                self.logger.error(e)
+            sleep(10)
+
+    def run(self):
+        worker_thread = threading.Thread(target=self.uploader, daemon=True)
+        worker_thread.start()
+
+    def __upload__(self, items):
+        headers = {
+            "Authorization": f"Bearer {self.api_token}",
+            "Content-Type": "application/json",
+        }
+        response = requests.post(self.api_url, headers=headers, json=items)
+        response.raise_for_status()
