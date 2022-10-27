047d421e912b49c3b4b3439eb2f5322c1325c459
Tjaart van der Walt <git@tjaart.org>
2022-10-27 15:42:28 -0700

    Add project.scripts (#20)

--- delta/__init__.py
+++ delta/__init__.py
@@ -1,1 +1,18 @@
 __version__ = "0.0.1"
+
+
+def main():
+    import os
+    import sys
+
+    if os.geteuid() == 0:
+        from delta.runner import Runner
+        from delta.uploader import Uploader
+
+        uploader = Uploader()
+        uploader.run()
+
+        runner = Runner()
+        runner.run()
+    else:
+        print("This script needs to be run as root.", file=sys.stderr)
