25fbbc4a4a92f993acf4d1844867612670e9d735
Andy Rusch <arusch@outsideopen.com>
2023-02-07 14:37:40 -0800

    Fixed error

--- delta/collectors/netdiscover/__init__.py
+++ delta/collectors/netdiscover/__init__.py
@@ -45,9 +45,9 @@ class Netdiscover(Collector):
     @staticmethod
     def should_run():
         last_run = Netdiscover.get_last_run(LAST_RUN_FILE)
-        2_days_ago = (datetime.now() - timedelta(days=2)).timestamp()
+        two_days_ago = (datetime.now() - timedelta(days=2)).timestamp()
 
-        if last_run < 2_days_ago:
+        if last_run < two_days_ago:
             return SHOULD_RUN
         else:
             return False
